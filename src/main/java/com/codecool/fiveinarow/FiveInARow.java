9edaa1aeefac5e866ff6009c99978adb8110a95d
wadkan <zsoltgaramszegi@gmail.com>
2020-08-24 17:24:32 +0200

    get user input and validate DONE

--- src/main/java/com/codecool/fiveinarow/FiveInARow.java
+++ src/main/java/com/codecool/fiveinarow/FiveInARow.java
@@ -4,6 +4,9 @@ public class FiveInARow {
 
     public static void main(String[] args) {
         Game game = new Game(11, 11);
+
+        game.getMove(1);
+
         game.enableAi(1);
         game.enableAi(2);
         game.play(5);
--- src/main/java/com/codecool/fiveinarow/Game.java
+++ src/main/java/com/codecool/fiveinarow/Game.java
@@ -1,11 +1,14 @@
 package com.codecool.fiveinarow;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Game implements GameInterface {
 
     private int[][] board;
 
     public Game(int nRows, int nCols) {
-        int[][] board = new int[nRows][nCols];
+        board = new int[nRows][nCols];
         for (int row = 0; row < nRows; row++) {
             for (int col = 0; col < nCols; col++) {
                 board[row][col] = 0;
@@ -22,7 +25,59 @@ public class Game implements GameInterface {
     }
 
     public int[] getMove(int player) {
-        return null;
+//        If the user provides coordinates that are outside of board, keep asking
+//        If the user provides coordinates for a place that is taken, keep asking
+//        If the user provides input that doesn't look like coordinates, keep asking
+
+        // char[] columnsLetters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'};
+        String columnsLetters = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+        char inputRow, inputRowRaw;
+        int inputCol = 0;
+        int inputRowNumber = 0;
+        boolean keepAsking = true;
+        int[] coordinates = new int[2];
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (keepAsking) {
+            try {
+                System.out.println("Please, give the row, eg.: A ->");
+                inputRowRaw = scanner.next().charAt(0);
+                inputRow = Character.toUpperCase(inputRowRaw);
+                inputRowNumber = columnsLetters.indexOf(inputRow);
+                System.out.println("Please, give the column, eg.: 1 ->");
+                inputCol = scanner.nextInt();
+            } catch (Exception e) {
+                keepAsking = true;
+                System.out.println("Don't kidding me, dude!");
+                continue;
+            }
+
+            if (inputRowNumber > board[0].length) {
+                keepAsking = true;
+                System.out.println("Wrong column letter!");
+            } else {
+                keepAsking = false;
+            }
+            if (inputCol > board.length) {
+                keepAsking = true;
+                System.out.println("Wrong row number!");
+            } else {
+                keepAsking = false;
+            }
+
+            System.out.println("row: " + inputRowNumber);
+            System.out.println("col: " + inputCol);
+            if (board[inputRowNumber][inputCol] != 0) {
+                keepAsking = true;
+                System.out.println("This field is not empty!");
+            }
+            ;
+        }
+
+        coordinates[0] = inputRowNumber;
+        coordinates[1] = inputCol;
+        return coordinates;
     }
 
     public int[] getAiMove(int player) {
@@ -30,6 +85,7 @@ public class Game implements GameInterface {
     }
 
     public void mark(int player, int row, int col) {
+
     }
 
     public boolean hasWon(int player, int howMany) {
